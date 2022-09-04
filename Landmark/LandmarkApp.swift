8d9d8c91c66b45b23c401e2a174ac924aa53fedc
Weston Mitchell <westonm127@gmail.com>
2022-09-03 22:27:56 -0700

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Weston Mitchell on 9/3/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
