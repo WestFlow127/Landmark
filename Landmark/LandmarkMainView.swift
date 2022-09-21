d0cd138e7a44b097264251dc570c5e940bfad42b
Weston Mitchell <westonm127@gmail.com>
2022-09-21 13:41:45 -0700

    removed nagivationView since LoginMainView has one

--- Landmark/LandmarkMainView.swift
+++ Landmark/LandmarkMainView.swift
@@ -12,62 +12,53 @@ import MapKit
 struct LandmarkMainView: View {
     @StateObject private var viewModel = LandmarkMainViewModel()
     @EnvironmentObject var loginViewModel: LoginViewModel
-
-    var body: some View {
-        NavigationView {
     
-            if loginViewModel.signedIn {
-                VStack {
-                    ViewThatFits {
-                        Map(coordinateRegion: $viewModel.region,
-                            showsUserLocation: true,
-                            annotationItems: viewModel.landmarks) { landmark in
-                            
-                                MapAnnotation(coordinate: landmark._2DCoord) {
-                                    Rectangle().stroke(Color.blue)
-                                        .frame(width: 20, height: 20)
-                                }
-                        }
-                            .onAppear{
-                                viewModel.checkIfLocationServicesIsEnabled()
-                            }
+    
+    var body: some View {
+        VStack {
+            ViewThatFits {
+                Map(coordinateRegion: $viewModel.region,
+                    showsUserLocation: true,
+                    annotationItems: viewModel.landmarks) { landmark in
+                    
+                    MapAnnotation(coordinate: landmark._2DCoord) {
+                        Rectangle().stroke(Color.blue)
+                            .frame(width: 20, height: 20)
                     }
                 }
-                .ignoresSafeArea()
-                .navigationBarTitleDisplayMode(NavigationBarItem.TitleDisplayMode.inline)
-                .toolbar {
-                    ToolbarItem(placement: .principal) {
-                        Text("Landmark")
-                            .font(.largeTitle)
-                            .bold()
+                    .onAppear{
+                        viewModel.checkIfLocationServicesIsEnabled()
                     }
-                    ToolbarItem (placement: .navigationBarTrailing){
-                        Menu {
-                            Button {
-                                loginViewModel.logout()
-                            } label: {
-                                Text("Logout")
-                            }
-                        } label: {
-                            Label {
-                                Text("Add")
-                            } icon: {
-                                Image(systemName: "plus")
-                                    .padding(.trailing, 7)
-                            }
-                        }
-                        .frame(width: 35, height: 35, alignment: .center)
-                        .background(Color.white)
-                        .cornerRadius(25)
-                    }
-                }
-            } else {
-                LoginMainView()
             }
         }
-        .onAppear{
-            loginViewModel.signedIn = loginViewModel.authManager.isSignedIn
+        .toolbar {
+            ToolbarItem(placement: .principal) {
+                Text("Landmark")
+                    .font(.largeTitle)
+                    .bold()
+            }
+            ToolbarItem (placement: .navigationBarTrailing){
+                Menu {
+                    Button {
+                        loginViewModel.logout()
+                    } label: {
+                        Text("Logout")
+                    }
+                } label: {
+                    Label {
+                        Text("Add")
+                    } icon: {
+                        Image(systemName: "plus")
+                            .padding(.trailing, 7)
+                    }
+                }
+                .frame(width: 35, height: 35, alignment: .center)
+                .background(Color.white)
+                .cornerRadius(25)
+            }
         }
+        .ignoresSafeArea()
+        .navigationBarTitleDisplayMode(NavigationBarItem.TitleDisplayMode.inline)
     }
 }
 
