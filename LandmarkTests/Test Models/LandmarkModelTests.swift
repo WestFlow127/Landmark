c655959c7e3970d7c1239e0c9f108ce6a7cabb61
Weston Mitchell <westonm127@gmail.com>
2023-09-25 19:17:40 -0700

    Create user unit tests (#29)
    * Create user test cases

--- LandmarkTests/Test Models/LandmarkModelTests.swift
+++ LandmarkTests/Test Models/LandmarkModelTests.swift
@@ -62,12 +62,4 @@ class LandmarkModelTests: XCTestCase {
 
         XCTAssertNotEqual(landmark, landmark2)
     }
-    
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
